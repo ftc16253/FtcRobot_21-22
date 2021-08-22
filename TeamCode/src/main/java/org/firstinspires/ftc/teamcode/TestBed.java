65c81665c0c91c885c2be66d7da5be69ba978cb2
Denise <denisem0712@yahoo.com>
2021-08-22 15:29:00 -0600

    Team meeting 8/22/2021

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBed.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+class TestBed extends LinearOpMode {
+
+    public void runOpMode() {
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+        }
+
+    }
+}
