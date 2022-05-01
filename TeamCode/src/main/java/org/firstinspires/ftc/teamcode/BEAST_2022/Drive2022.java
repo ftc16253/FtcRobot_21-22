9b62a761a93f5cf06aae7c763d820bf105864986
Denise <denisem0712@yahoo.com>
2022-05-01 12:50:51 -0600

    4/29/2022

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BEAST_2022/Drive2022.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BEAST_2022/Drive2022.java
@@ -1,21 +1,49 @@
 package org.firstinspires.ftc.teamcode.BEAST_2022;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp
 public class Drive2022 extends LinearOpMode {
+    Pushbot2022 bot = new Pushbot2022();
     public void runOpMode() {
-         DcMotor frontLeftMec, frontRightMec, backRightMec, backLeftMec;
-        frontLeftMec  = hardwareMap.get(DcMotor.class, "frontLeftMec");
-        frontRightMec = hardwareMap.get(DcMotor.class, "frontRightMec");
-        backLeftMec  =hardwareMap.get(DcMotor.class, "backLeftMec");
-        backRightMec = hardwareMap.get(DcMotor.class, "backRightMec");
+        bot.init(hardwareMap);
 
-        frontLeftMec.setDirection(DcMotor.Direction.REVERSE);
-        frontRightMec.setDirection(DcMotor.Direction.FORWARD);
-        backLeftMec.setDirection(DcMotor.Direction.FORWARD);
-        backRightMec.setDirection(DcMotor.Direction.REVERSE);
+        waitForStart();
+
+        while (opModeIsActive()){
+            if (gamepad1.right_stick_y != 0){
+                bot.moveForward(gamepad1.right_stick_y);
+            }
+            if (gamepad1.right_stick_x != 0){
+                bot.turn(gamepad1.right_stick_x);
+            }
+            if (gamepad1.left_stick_x > 0 && gamepad1.left_stick_y > 0) {
+                //move forward diagonally to the right
+                bot.frontRightMec.setPower(1);
+                bot.backLeftMec.setPower(1);
+            }else if (gamepad1.left_stick_x < 0 && gamepad1.left_stick_y < 0) {
+                //move backward diagonally to the left
+                bot.frontRightMec.setPower(-1);
+                bot.backLeftMec.setPower(-1);
+            }else if (gamepad1.left_stick_x > 0 && gamepad1.left_stick_y < 0) {
+                //move backward diagonally to the right
+                bot.frontLeftMec.setPower(-1);
+                bot.backRightMec.setPower(-1);
+            }else if (gamepad1.left_stick_x < 0 && gamepad1.left_stick_y > 0) {
+                //move forward diagonally to the left
+                bot.frontLeftMec.setPower(1);
+                bot.backRightMec.setPower(1);
+            }else if (gamepad1.left_stick_x != 0){
+                bot.moveSide(gamepad1.left_stick_x);
+            }
+            else{
+                bot.frontRightMec.setPower(0);
+                bot.frontLeftMec.setPower(0);
+                bot.backLeftMec.setPower(0);
+                bot.backRightMec.setPower(0);
+            }
+        }
     }
-
 }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BEAST_2022/Pushbot2022.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BEAST_2022/Pushbot2022.java
@@ -2,24 +2,71 @@ package org.firstinspires.ftc.teamcode.BEAST_2022;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-class Pushbot2022 extends LinearOpMode {
+import org.firstinspires.ftc.teamcode.BEAST_2021.DuckDetector;
+import org.openftc.easyopencv.OpenCvCamera;
 
-    public void runOpMode() {
-        DcMotor frontLeftMec, frontRightMec, backRightMec, backLeftMec;
-        frontLeftMec = hardwareMap.get(DcMotor.class, "frontLeftMec");
-        frontRightMec = hardwareMap.get(DcMotor.class, "frontRightMec");
-        backLeftMec = hardwareMap.get(DcMotor.class, "backLeftMec");
-        backRightMec = hardwareMap.get(DcMotor.class, "backRightMec");
+class Pushbot2022{
+    /* Public OpMode members. */
+    public DcMotor frontLeftMec, frontRightMec, backRightMec, backLeftMec;
+    double spinDiameter = 1;
+    double diameter = 3.6;
+    double circumference = diameter * 3.14;
+    double andyMark40Tics = 1120;
+    double andyMark20Tics = 537.6;
+    public static final String VUFORIA_KEY =
+            "Afctxlz/////AAABmSWf4jOsTUHcsOYa/JfaZlRo+3yiPN8cCUH4BDLpIZ8FAt0tEVLJ/mxWUyd7f0gqd+a7JRTMYP9+A9s1nojOs9B1ZGOFsvr84RZnbVN8cGP7RFKNP4Mg0Pr/6vIUmHGFx/jrOrXz/YJXwVXvPpqr1uDm8xpBZOE4j+CtQcKW2Y2zjVWHWRTkmb6ve/R91k3jfjaH4PErbZMcvD7Xy5IesqSet3/pjeUXWSnlHmPwH7fgUcHSkAf0Fj2nLvZ7zmpT8vh9rSKri9XD3A64WBNRO+6+SGH/C/eS3mWLmdi5ZMbSK66WuvNhAPT0SHCzzqAlAf2P6asrrrAuw+aQ0B2HV0mPtGdNPe62djhu5Afa/rL+";
 
-        frontLeftMec.setDirection(DcMotor.Direction.REVERSE);
-        frontRightMec.setDirection(DcMotor.Direction.FORWARD);
+
+    /* local OpMode members. */
+
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    public void init(HardwareMap ahwMap) {
+        hwMap = ahwMap;
+        //DcMotor frontLeftMec, frontRightMec, backRightMec, backLeftMec;
+        frontLeftMec = hwMap.get(DcMotor.class, "frontLeftMec");
+        frontRightMec = hwMap.get(DcMotor.class, "frontRightMec");
+        backLeftMec = hwMap.get(DcMotor.class, "backLeftMec");
+        backRightMec = hwMap.get(DcMotor.class, "backRightMec");
+
+        frontLeftMec.setDirection(DcMotor.Direction.FORWARD);
+        frontRightMec.setDirection(DcMotor.Direction.REVERSE);
         backLeftMec.setDirection(DcMotor.Direction.FORWARD);
         backRightMec.setDirection(DcMotor.Direction.REVERSE);
 
+        //set power to zero
+        frontLeftMec.setPower(0);
+        frontRightMec.setPower(0);
+        backLeftMec.setPower(0);
+        backRightMec.setPower(0);
     }
-    public void moveFowardInch(double distance){
+    public void moveForward(double power){
+        frontLeftMec.setPower(power);
+        frontRightMec.setPower(power);
+        backLeftMec.setPower(power);
+        backRightMec.setPower(power);
+    }
+    public void turn(double power){
+        frontRightMec.setPower(power);
+        frontLeftMec.setPower(-power);
+        backRightMec.setPower(power);
+        backLeftMec.setPower(-power);
+    }
+    public void moveSide(double power){
+        frontLeftMec.setPower(-power);
+        frontRightMec.setPower(power);
+        backLeftMec.setPower(power);
+        backRightMec.setPower(-power);
+    }
+    public void moveFowardInch(double distance, double power){
+    frontLeftMec.setPower(power);
+    }
 
-    }
 }
 
